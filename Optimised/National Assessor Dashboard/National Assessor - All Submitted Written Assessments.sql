902deacbdca92e023f0eed9500985d93a86e269d
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2022-10-26 12:40:10 +1300

    fixed queries

--- Optimised/National Assessor Dashboard/National Assessor - All Submitted Written Assessments.sql
+++ Optimised/National Assessor Dashboard/National Assessor - All Submitted Written Assessments.sql
@@ -1,10 +1,9 @@
 SELECT
 CONCAT(u.firstname, ' ', u.lastname) AS Learner,
-CONCAT('<a target="_new" href = "%%WWWROOT%%/course/view.php?id=', CAST(c.id AS CHAR), '">',CAST(c.fullname AS CHAR),'</a>') AS 'Course',
-quiz.name,
+/*CONCAT('<a target="_new" href = "%%WWWROOT%%/course/view.php?id=', CAST(c.id AS CHAR), '">',CAST(c.fullname AS CHAR),'</a>') AS 'Course',*/
+gquiz.quizid,
 attempts.attempt AS 'Attempt',
-attempts.id,
-lprog.programid
+attempts.id
 /*
 IF(qover.attempts IS NULL, '1', qover.attempts) AS 'Attempts allocated',
 CONCAT('<a target="_new" href = "%%WWWROOT%%/mod/quiz/review.php?attempt=', CAST(attempts.id AS CHAR), '">View attempt</a>') AS 'View attempt',
@@ -14,19 +13,13 @@ ROUND(attempts.sumgrades/quiz.sumgrades*100,1) AS 'Grade',
 CONCAT(IF(attempts.timefinish = 0, 'In progress', ''), IF(attempts.timefinish != 0 AND attempts.sumgrades IS NULL, 'Submitted', ''), IF(attempts.sumgrades IS NOT NULL, 'Graded', '')) AS 'Status'
 */
 
-FROM prefix_block_mitoassessor_quizstatus AS quizattempt
+FROM mdl_block_mitoassessor_tobegraded AS gquiz
 
-JOIN prefix_user AS u
-	ON quizattempt.userid = u.id
-JOIN prefix_quiz AS quiz
-	ON quizattempt.quizid = quiz.id
-JOIN prefix_block_mitoassessor_learnercompl AS lprog
-	ON c.id = lprog.courseid
-	AND quizattempt.userid = lprog.userid
-JOIN prefix_course AS c
-	ON quiz.course = c.id
-LEFT JOIN prefix_quiz_attempts AS attempts
-	ON u.id = attempts.userid
-	AND quiz.id = attempts.quiz
+JOIN mdl_quiz_attempts AS attempts
+	ON gquiz.attemptid = attempts.id
+	AND gquiz.quizid = attempts.quiz
+	AND gquiz.userid = attempts.userid
 
-ORDER BY attempts.timemodified DESC
+JOIN mdl_user AS u
+	ON gquiz.userid = u.id
+
