43b570f5e5ebe92a0c4417f3a086ed1bb3bf5aa3
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2022-10-26 16:20:54 +1300

    more optimised and originals backed up

--- Optimised/National Assessor Dashboard/National Assessor - All Submitted Written Assessments.sql
+++ Optimised/National Assessor Dashboard/National Assessor - All Submitted Written Assessments.sql
@@ -1,25 +1,51 @@
-SELECT
+SELECT DISTINCT 
 CONCAT(u.firstname, ' ', u.lastname) AS Learner,
-/*CONCAT('<a target="_new" href = "%%WWWROOT%%/course/view.php?id=', CAST(c.id AS CHAR), '">',CAST(c.fullname AS CHAR),'</a>') AS 'Course',*/
-gquiz.quizid,
+CONCAT('<a target="_new" href = "%%WWWROOT%%/course/view.php?id=', CAST(c.id AS CHAR), '">',CAST(c.fullname AS CHAR),'</a>') AS 'Course',
+quiz.name,
 attempts.attempt AS 'Attempt',
-attempts.id
-/*
+attempts.id,
 IF(qover.attempts IS NULL, '1', qover.attempts) AS 'Attempts allocated',
 CONCAT('<a target="_new" href = "%%WWWROOT%%/mod/quiz/review.php?attempt=', CAST(attempts.id AS CHAR), '">View attempt</a>') AS 'View attempt',
 CONCAT('Submitted - ', '<span class="accesshide" >', CAST(attempts.timefinish as CHAR), '</span>', DATE_FORMAT(FROM_UNIXTIME(attempts.timefinish),'%d %M %Y')) AS Submitted,
 DATE_FORMAT(FROM_UNIXTIME(attempts.timefinish),'%d/%m/%Y') AS D,
-ROUND(attempts.sumgrades/quiz.sumgrades*100,1) AS 'Grade',
-CONCAT(IF(attempts.timefinish = 0, 'In progress', ''), IF(attempts.timefinish != 0 AND attempts.sumgrades IS NULL, 'Submitted', ''), IF(attempts.sumgrades IS NOT NULL, 'Graded', '')) AS 'Status'
-*/
+ROUND(attempts.sumgrades/quiz.sumgrades*100,1) AS 'Grade'
 
-FROM mdl_block_mitoassessor_tobegraded AS gquiz
+FROM prefix_quiz_attempts AS attempts
+JOIN prefix_quiz AS quiz 
+	ON attempts.quiz = quiz.id
+JOIN prefix_course_modules AS cm 
+	ON cm.instance = quiz.id
+    AND quiz.course = cm.course
+JOIN prefix_user AS u 
+	ON u.id = attempts.userid
+LEFT JOIN prefix_quiz_overrides AS qover 
+	ON qover.quiz = quiz.id 
+    AND qover.userid = u.id
+JOIN prefix_course AS c 
+	ON c.id = cm.course
+JOIN prefix_course_categories AS cc 
+	ON cc.id = c.category
+LEFT JOIN prefix_prog_user_assignment AS progua 
+	ON progua.userid = u.id 
+LEFT JOIN prefix_prog AS prog 
+	ON prog.id = progua.programid
+LEFT JOIN prefix_course_modules_completion cmc 
+	ON cmc.userid = u.id 
+    AND cmc.coursemoduleid = cm.id
 
-JOIN mdl_quiz_attempts AS attempts
-	ON gquiz.attemptid = attempts.id
-	AND gquiz.quizid = attempts.quiz
-	AND gquiz.userid = attempts.userid
+WHERE 
+quiz.preferredbehaviour = 'deferredfeedback'
 
-JOIN mdl_user AS u
-	ON gquiz.userid = u.id
+AND u.suspended = 0
+AND prog.fullname IS NOT NULL
+AND cc.name != 'COF'
 
+/*Admin users, Demo learners and test users are hidden from this queue*/
+AND u.username != 'elearning.admin'  AND u.username != 'mito1' AND u.username != 'mito2' AND u.username != 'mito_nzcr' AND u.username != 'mito_nzar' AND u.username != 'mito1' AND u.username != 'mito_nzlv' AND u.username != 'mito_suig'
+AND u.username != '773294@mymitonz.org.nz' AND u.username != '773288@mymitonz.org.nz' AND u.username != '773290@mymitonz.org.nz' AND u.username != 'demolearner'
+AND u.username != 'gbalasuriya' AND u.username != 'hclark' AND u.username != 'kahmad' 
+/**/
+
+/*%%FILTER_SEARCHTEXT:(Concat(u.firstname, ' ', u.lastname, ' ', u.username)):~%%*/
+
+ORDER BY attempts.timefinish DESC
--- Originals/National Assessor Dashboard/National Assessor - All Submitted Written Assessments.sql
+++ Originals/National Assessor Dashboard/National Assessor - All Submitted Written Assessments.sql
@@ -1,30 +1,42 @@
-SELECT
-CONCAT(u.firstname,' ',u.lastname) AS 'Learner',
+SELECT DISTINCT 
+CONCAT(u.firstname, ' ', u.lastname) AS Learner,
 CONCAT('<a target="_new" href = "%%WWWROOT%%/course/view.php?id=', CAST(c.id AS CHAR), '">',CAST(c.fullname AS CHAR),'</a>') AS 'Course',
 quiz.name,
 attempts.attempt AS 'Attempt',
 attempts.id,
-attempts.attempt AS 'Attempts allocated',
+IF(qover.attempts IS NULL, '1', qover.attempts) AS 'Attempts allocated',
 CONCAT('<a target="_new" href = "%%WWWROOT%%/mod/quiz/review.php?attempt=', CAST(attempts.id AS CHAR), '">View attempt</a>') AS 'View attempt',
 CONCAT('Submitted - ', '<span class="accesshide" >', CAST(attempts.timefinish as CHAR), '</span>', DATE_FORMAT(FROM_UNIXTIME(attempts.timefinish),'%d %M %Y')) AS Submitted,
 DATE_FORMAT(FROM_UNIXTIME(attempts.timefinish),'%d/%m/%Y') AS D,
 ROUND(attempts.sumgrades/quiz.sumgrades*100,1) AS 'Grade',
 CONCAT(IF(attempts.timefinish = 0, 'In progress', ''), IF(attempts.timefinish != 0 AND attempts.sumgrades IS NULL, 'Submitted', ''), IF(attempts.sumgrades IS NOT NULL, 'Graded', '')) AS 'Status'
 
-FROM mdl_block_mitoassessor_tobegraded AS gquiz
-LEFT JOIN mdl_quiz_overrides AS qover
-    ON gquiz.quizid = qover.quiz
-    AND gquiz.userid = qover.userid
-JOIN mdl_quiz_attempts AS attempts
-	ON gquiz.attemptid = attempts.id
-JOIN mdl_course AS c
-	ON gquiz.courseid = c.id
-JOIN mdl_quiz AS quiz
-	ON gquiz.quizid = quiz.id
-JOIN mdl_user AS u
-	ON gquiz.userid = u.id
-    AND u.suspended = 0
-    
-WHERE quiz.preferredbehaviour = 'deferredfeedback'
-AND u.username != 'elearning.admin'
+FROM prefix_quiz_attempts attempts
+JOIN prefix_quiz quiz ON attempts.quiz=quiz.id
+JOIN prefix_course_modules cm ON cm.instance=quiz.id
+JOIN prefix_user u ON u.id = attempts.userid
+LEFT JOIN prefix_quiz_overrides qover ON qover.quiz = quiz.id AND qover.userid = u.id
+JOIN prefix_course c ON c.id = cm.course
+JOIN prefix_course_categories AS cc ON cc.id = c.category
+JOIN prefix_grade_items gi ON gi.iteminstance = cm.instance AND gi.courseid = c.id
+JOIN prefix_grade_grades gg ON gg.itemid = gi.id AND gg.userid = u.id
+LEFT JOIN prefix_prog_user_assignment AS progua ON progua.userid = u.id 
+LEFT JOIN prefix_prog AS prog ON prog.id = progua.programid
+LEFT JOIN prefix_course_modules_completion cmc ON cmc.userid = u.id AND cmc.coursemoduleid = cm.id
+
+WHERE 
+quiz.preferredbehaviour = 'deferredfeedback'
+
+AND u.suspended = 0
+AND prog.fullname IS NOT NULL
+AND cc.name != 'COF'
+
+/*Admin users, Demo learenrs and test users are hidden from this queue*/
+AND u.username != 'elearning.admin'  AND u.username != 'mito1' AND u.username != 'mito2' AND u.username != 'mito_nzcr' AND u.username != 'mito_nzar' AND u.username != 'mito1' AND u.username != 'mito_nzlv' AND u.username != 'mito_suig'
+AND u.username != '773294@mymitonz.org.nz' AND u.username != '773288@mymitonz.org.nz' AND u.username != '773290@mymitonz.org.nz' AND u.username != 'demolearner'
+AND u.username != 'gbalasuriya' AND u.username != 'hclark' AND u.username != 'kahmad' 
+/**/
+
+%%FILTER_SEARCHTEXT:(Concat(u.firstname, ' ', u.lastname, ' ', u.username)):~%%
+
 ORDER BY attempts.timefinish DESC
